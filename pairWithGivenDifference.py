cf457b507150e2bd007c76d6eb2b96a3c7eee322
Syfuddeen <syfuddeen107@gmail.com>
2020-09-28 22:38:08 +0530

    Add files via upload

---
+++ pairWithGivenDifference.py
@@ -0,0 +1,19 @@
+def pair(arr, n, difference):
+    arr.sort()
+    i, j = 0, 1
+    while i < n and j < n:
+        if arr[j] - arr[i] == difference:
+            print('pair : {0} {1}'.format(arr[i], arr[j]))
+            return 1
+        elif arr[j] - arr[i] > difference:
+            i += 1
+        elif arr[j] - arr[i] < difference:
+            j += 1
+    print('no pair found')
+
+
+if __name__ == '__main__':
+    arr = list(map(int, input().strip().split()))
+    difference = int(input())
+    n = len(arr)
+    pair(arr, n, difference)
